92d5fe51c9e1784bd71f1f4195cd6d76f9146d78
James Elliott <james@deepsymmetry.org>
2017-03-20 22:16:41 -0500

    Prepare for new development with latest beat-link build.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beat-carabiner "0.1.0"
+(defproject beat-carabiner "0.1.1-SNAPSHOT"
   :description "A minimal tempo bridge between Pioneer Pro DJ Link and Ableton Link."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -7,7 +7,7 @@
                  [org.clojure/tools.cli "0.3.5"]
                  [com.taoensso/timbre "4.8.0"]
                  [com.fzakaria/slf4j-timbre "0.3.4"]
-                 [org.deepsymmetry/beat-link "0.2.1"]]
+                 [org.deepsymmetry/beat-link "0.2.2-SNAPSHOT"]]
   :main ^:skip-aot beat-carabiner.core
   :uberjar-name "beat-carabiner.jar"
   :manifest {"Name" ~#(str (clojure.string/replace (:group %) "." "/")

6cf39431fa9317216414c3ce0af7082a43b4293a
Erik Minekus <tsunami@tsunami-productions.nl>
2017-12-11 18:02:00 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [com.taoensso/timbre "4.8.0"]
-                 [com.fzakaria/slf4j-timbre "0.3.4"]
-                 [org.deepsymmetry/beat-link "0.2.2-SNAPSHOT"]]
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.12"]
+                 [org.deepsymmetry/beat-link "0.4.0"]]
   :main ^:skip-aot beat-carabiner.core
   :uberjar-name "beat-carabiner.jar"
   :manifest {"Name" ~#(str (clojure.string/replace (:group %) "." "/")

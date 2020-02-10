5dc4c7076e2bba7c8468ddae68c0b60806fc7f73
James Elliott <james@deepsymmetry.org>
2020-02-09 19:44:43 -0600

    Add SCM information for cljdoc.org

--- project.clj
+++ project.clj
@@ -2,19 +2,20 @@
   :description "A minimal tempo bridge between Beat Link and Ableton Link."
   :url "https://github.com/Deep-Symmetry/beat-carabiner"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git" :url "https://github.com/Deep-Symmetry/beat-carabiner"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.deepsymmetry/beat-link "0.6.1"]
                  [org.deepsymmetry/lib-carabiner "1.1.3"]
                  [org.deepsymmetry/electro "0.1.3"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.fzakaria/slf4j-timbre "0.3.19"]]
-  :manifest {"Name" ~#(str (clojure.string/replace (:group %) "." "/")
-                            "/" (:name %) "/")
-             "Package" ~#(str (:group %) "." (:name %))
-             "Specification-Title" ~#(:name %)
+  :manifest {"Name"                  ~#(str (clojure.string/replace (:group %) "." "/")
+                                            "/" (:name %) "/")
+             "Package"               ~#(str (:group %) "." (:name %))
+             "Specification-Title"   ~#(:name %)
              "Specification-Version" ~#(:version %)}
   :target-path "target/%s"
   :profiles {:dev {:repl-options {:init-ns beat-carabiner.core
                                   :welcome (println "beat-carabiner loaded.")}
-                   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]}})
+                   :jvm-opts     ["-XX:-OmitStackTraceInFastThrow"]}})

dc06fba522174a4dcf65b020d94f65e3782cdf1e
James Elliott <james@deepsymmetry.org>
2020-07-01 22:05:32 -0500

    Update to new release of lib-carabiner.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject beat-carabiner "0.2.2"
+(defproject beat-carabiner "0.2.3-SNAPSHOT"
   :description "A minimal tempo bridge between Beat Link and Ableton Link."
   :url "https://github.com/Deep-Symmetry/beat-carabiner"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :scm {:name "git" :url "https://github.com/Deep-Symmetry/beat-carabiner"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.deepsymmetry/beat-link "0.6.2"]
-                 [org.deepsymmetry/lib-carabiner "1.1.3"]
+                 [org.deepsymmetry/lib-carabiner "1.1.4"]
                  [org.deepsymmetry/electro "0.1.3"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.fzakaria/slf4j-timbre "0.3.19"]]

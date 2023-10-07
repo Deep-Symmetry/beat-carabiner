2e0c92bb6b748d708e82ccf6a0a1bb3d13fe256d
James Elliott <james@deepsymmetry.org>
2023-10-07 13:27:16 -0500

    Update dependencies, change version to track BLT's.

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject beat-carabiner "1.0.0"
+(defproject beat-carabiner "7.3.0-SNAPSHOT"
   :description "A minimal tempo bridge between Beat Link and Ableton Link."
   :url "https://github.com/Deep-Symmetry/beat-carabiner"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git" :url "https://github.com/Deep-Symmetry/beat-carabiner"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.deepsymmetry/beat-link "7.0.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.deepsymmetry/beat-link "7.3.0-SNAPSHOT"]
                  [org.deepsymmetry/lib-carabiner "1.1.6"]
                  [org.deepsymmetry/electro "0.1.4"]
-                 [com.taoensso/timbre "5.1.2"]
-                 [com.fzakaria/slf4j-timbre "0.3.21"]]
+                 [com.taoensso/timbre "6.3.1"]
+                 [com.fzakaria/slf4j-timbre "0.4.0"]]
   :manifest {"Name"                  ~#(str (clojure.string/replace (:group %) "." "/")
                                             "/" (:name %) "/")
              "Package"               ~#(str (:group %) "." (:name %))
